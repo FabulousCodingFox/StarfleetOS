b0fe6dd92176dc9349fdfea00feb6f0ccfd681f3
FabulousFox <you@example.com>
2023-07-05 07:18:09 +0200

    Bios Parameter Block & Prefilled Registries

--- src/boot.asm
+++ src/boot.asm
@@ -1,7 +1,27 @@
-ORG 0x7c00
+ORG 0
 BITS 16
+; Bios Parameter Block
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
 
 start:
+    jmp 0x7c0:step2
+
+step2:
+    ; Clear all registered
+    cli
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+
+    ; Print 'Hello World'
+    sti
     mov si, message
     call print
     jmp $
@@ -22,7 +42,7 @@ print_char:
     int 0x10
     ret
 
-message: db 'Hello World!', 0
+message: db 'Loaded StarfleetOS Bootloader', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
