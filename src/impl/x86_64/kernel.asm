233cf3de9c15cb65d61b851674645cd1983e5ba6
FabulousFox <you@example.com>
2023-07-08 11:55:24 +0200

    p

--- src/impl/x86_64/kernel.asm
+++ src/impl/x86_64/kernel.asm
@@ -1,19 +1,22 @@
 [BITS 32]
+global _start
+
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
-load32:
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	mov ebp, 0x00200000
-	mov esp, ebp
 
-	; Enable A20 line
-	in al, 0x92
-	or al, 2
-	out 0x92, al
+_start:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
 
-	jmp $
+    ; Enable the A20 line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp $
